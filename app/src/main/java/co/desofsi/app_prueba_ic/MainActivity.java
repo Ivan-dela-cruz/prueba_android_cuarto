aeffa2f97ba328083ee5db3140918e1f4b30721e
ivan495 <canarivan495@gmail.com>
2020-01-24 07:41:25 -0500

    inicoio de la prueba

---
+++ app/src/main/java/co/desofsi/app_prueba_ic/MainActivity.java
@@ -0,0 +1,14 @@
+package co.desofsi.app_prueba_ic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
